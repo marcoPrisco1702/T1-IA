6bcaba98d55409eabcd324f506796f9b518a91ba
marquinthecreator <marco.prisco@edu.pucrs.br>
2025-09-30 18:02:28 -0300

    pequenos comentarios

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from NhacNhac import Jogo
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--prof", type=int, default=4, help="Profundidade máxima da IA") # a padrao é 4, mas pode mudar no terminal ali pra testes 
+    parser.add_argument("--prof", type=int, default=4, help="Profundidade máxima da IA") # a padrao é 4, mas pode mudar no terminal ali pra testes só na hora de colocar tem que ser tipo: --prof x --tempo x 
     parser.add_argument("--tempo", type=float, default=30.0, help="Limite de tempo por jogada em segundos") #  a padrao é 30 seg, mas pode mudar no terminal ali pra testes
     args = parser.parse_args()
 
